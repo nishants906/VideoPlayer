22c53910e6e39e8b2ccbd2c38b21775148e66f77
nishants12 <sardana.nishant@gmail.com>
2017-02-14 11:59:02 +0530

    seek_preview_added

--- app/src/main/java/first/com/movie_player/MovieList.java
+++ app/src/main/java/first/com/movie_player/MovieList.java
@@ -6,26 +6,38 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.widget.MediaController;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
+import com.github.rubensousa.previewseekbar.PreviewSeekBar;
 import com.swipper.library.Swipper;
 
 import java.util.List;
 
-public class MovieList extends Swipper implements SensorEventListener{
+public class MovieList extends Swipper implements SensorEventListener, View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     Uri uri;
     VideoView videov;
     Context context;
     DBHandler db;
     List<String> location=null;
-    MediaController mc;
     int i=0;
+
+    PreviewSeekBar seekBar;
+    Button play;
+    Button pause;
+    Button forward;
+    Button back;
+    LinearLayout ll;
+
 
     private SensorManager mSensorManager;
     private Sensor mProximity;
@@ -36,9 +48,6 @@ public class MovieList extends Swipper implements SensorEventListener{
 
     public MovieList(Context context) {
         this.context = context;
-
-
-
     }
 
     @Override
@@ -48,6 +57,32 @@ public class MovieList extends Swipper implements SensorEventListener{
 
         set(this);
 
+        ll= (LinearLayout) findViewById(R.id.ll);
+        videov= (VideoView) findViewById(R.id.video);
+        seekBar= (PreviewSeekBar) findViewById(R.id.seek);
+        play= (Button) findViewById(R.id.play);
+        pause= (Button) findViewById(R.id.pause);
+        back= (Button) findViewById(R.id.back);
+        forward= (Button) findViewById(R.id.forward);
+
+        videov.setOnClickListener(this);
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        back.setOnClickListener(this);
+        forward.setOnClickListener(this);
+
+        videov.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                seekBar.setMax(videov.getDuration());
+                seekBar.postDelayed(onEverySecond, 1000);
+            }
+        });
+        seekBar.addOnSeekBarChangeListener(this);
+
+
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
@@ -55,11 +90,10 @@ public class MovieList extends Swipper implements SensorEventListener{
         location=db.access_data();
 
         getWindow().setFormat(PixelFormat.UNKNOWN);
-        mc=new MediaController(this);
         uri=uri.parse(location.get(i));
         Log.d("upro123", String.valueOf(uri));
-        videov= (VideoView) findViewById(R.id.video);
         videov.setVideoURI(uri);
+
 
         DisplayMetrics metrics = new DisplayMetrics(); getWindowManager().getDefaultDisplay().getMetrics(metrics);
         android.widget.RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) videov.getLayoutParams();
@@ -70,8 +104,6 @@ public class MovieList extends Swipper implements SensorEventListener{
         Brightness(Orientation.VERTICAL);
         Volume(Orientation.CIRCULAR);
         Seek(Orientation.HORIZONTAL,videov);
-        videov.setMediaController(mc);
-        mc.setAnchorView(videov);
         videov.start();
 
     }
@@ -109,6 +141,64 @@ public class MovieList extends Swipper implements SensorEventListener{
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.play:{
+                videov.start();
+                play.setVisibility(View.GONE);
+                pause.setVisibility(View.VISIBLE);
+                break;
+            }
+            case R.id.pause:{
+                videov.pause();
+                pause.setVisibility(View.GONE);
+                play.setVisibility(View.VISIBLE);
+                break;
+            }
+            case R.id.video:{
+                ll.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private Runnable onEverySecond=new Runnable() {
+
+        @Override
+        public void run() {
+
+            if(seekBar != null) {
+                seekBar.setProgress(videov.getCurrentPosition());
+            }
+
+            if(videov.isPlaying()) {
+                seekBar.postDelayed(onEverySecond, 1000);
+            }
+
+        }
+    };
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        if(fromUser) {
+            // this is when actually seekbar has been seeked to a new position
+            videov.seekTo(progress);
+        }
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
